487878ae67dd5c794c33284db5981590a4c46476
Yousef Sadegheih <yousef.sadegheih@ur.de>
2024-01-19 16:32:25 +0100

    Add continue_training flag to run_training.py script

--- training_scripts/run_training_acdc.sh
+++ training_scripts/run_training_acdc.sh
@@ -7,4 +7,4 @@ export RESULTS_FOLDER=/cabinet/yousef/ACDC-nnunet/output_acdc_lhunet_res_coll_ba
 export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task01_ACDC
 export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 
-python ../unetr_pp/run/run_training.py 3d_fullres unetr_pp_trainer_acdc 1 0
+python ../unetr_pp/run/run_training.py 3d_fullres unetr_pp_trainer_acdc 1 0 --continue_training
