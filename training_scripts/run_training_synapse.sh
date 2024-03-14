bd821e2ae71690699987b40ae5f8605557a90bbb
Yousef Sadegheih <yousef.sadegheih@ur.de>
2024-03-14 15:46:38 +0100

    .

--- training_scripts/run_training_synapse.sh
+++ training_scripts/run_training_synapse.sh
@@ -3,7 +3,7 @@
 DATASET_PATH=/cabinet/dataset/Synapse/NN-Unet/DATASET_Synapse_unetr_pp
 
 export PYTHONPATH=.././
-export RESULTS_FOLDER=/cabinet/yousef/Synapse-nnunet/output_synapse_unetr_pp
+export RESULTS_FOLDER=/cabinet/yousef/Synapse-nnunet/output_synapse_lhunet_BEST_kernels_3_dec_5
 export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
 export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 
